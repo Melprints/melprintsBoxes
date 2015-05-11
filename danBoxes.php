8abf921d1d3bfb5b3d738cbde99ef6dc8bc51888
thedudeap <dan@melprinting.com>
2015-05-11 11:53:43 -0400

    dans file

---
+++ danBoxes.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Dan Testing</title>
+
+<style type="text/css">
+.theBox:hover{
+	-webkit-transition-delay: 200ms;
+	-moz-transition-delay: 200ms;
+	-o-transition-delay: 200ms;
+	-ms-transition-delay: 200ms;
+	transition-delay: 200ms;
+	
+
+	transform: rotate3d(0,10,100,-3deg);
+	
+}
+</style>
+</head>
+
+<body>
+	<div class="theBox">
+		<img src="images/box1.jpg">
+    </div>
+</body>
+</html>
