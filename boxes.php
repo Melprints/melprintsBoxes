45cd00cec48bb0939d8f815f5ab46cdb9baeb1d6
alicave <alison@melprinting.com>
2015-05-14 13:37:07 -0400

    Box Update

---
+++ boxes.php
@@ -0,0 +1,55 @@
+<?php include "../header.php"; ?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Melprints Box Options</title>
+<style type="text/css">
+BODY {
+	margin:0;
+	padding:0;	
+}
+.clear {
+	clear:both;	
+}
+.boxWrapper {
+	width:960px;
+	margin:0 auto;	
+}
+.boxImages {
+	float:left;	
+}
+</style>
+</head>
+
+<body>
+	<div class="boxWrapper">
+	<div class="boxHeader"><h1>Melprints Box Options</h1></div>
+       
+       		<div class="boxSection">
+            <div class="boxImages"><img src="images/pizzaBox.png"/></div>
+				<div class="boxDescription">Pizza Box</div>
+            </div>
+        	
+            <div class="boxSection">
+            	<div class="boxImages"><img src="images/rettLocks.png"/></div>
+				<div class="boxDescription">Roll End Tuck Top with Locks</div>
+			</div>
+        	
+            <div class="boxSection">
+            <div class="boxImages"><img src="images/tuckTopWithFlap.jpg"/></div>
+				<div class="boxDescription">Tuck Top with Flap</div>
+			</div>
+            </div>
+        	
+            <div class="boxSection">
+            <div class="boxImages"><img src="images/melprintsBox.jpg"/></div>
+				<div class="boxDescription">Box Example</div>
+                
+			</div>
+            
+        </div><!-- end box wrapper -->
+        <div class="clear"></div>
+</body>
+</html>
+<?php include "../footer.php"; ?>
