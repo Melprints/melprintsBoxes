33388138c7d5714f5b340495c729e54a5f9ca3ea
alicave <alison@melprinting.com>
2015-05-14 15:22:38 -0400

    Boxes as of 5/14

--- boxes.php
+++ boxes.php
@@ -1,55 +1,45 @@
 <?php include "../header.php"; ?>
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Melprints Box Options</title>
+
 <style type="text/css">
 BODY {
 	margin:0;
-	padding:0;	
+	padding:0;
+	width:100%;
+	height:100%;
+	color:#121212;	
 }
 .clear {
 	clear:both;	
 }
-.boxWrapper {
-	width:960px;
-	margin:0 auto;	
+.boxHeader {
+	text-align:center;
+	color:#343434;
+	font-size:2em;
+	border-bottom:1px solid #ccc; 
+	margin:30px;
+	padding-bottom:20px;
 }
-.boxImages {
-	float:left;	
+
+.boxTypeImages {
+    display:inline-block;
+	float:left;
+	box-sizing:border-box;
+	border:4px solid #999;
+	width:320px;
+	height:320px;
+	margin: 20px 0px 0px 20px;
+	background-color:#f5f5f5;
 }
 </style>
-</head>
-
-<body>
-	<div class="boxWrapper">
-	<div class="boxHeader"><h1>Melprints Box Options</h1></div>
-       
-       		<div class="boxSection">
-            <div class="boxImages"><img src="images/pizzaBox.png"/></div>
-				<div class="boxDescription">Pizza Box</div>
-            </div>
+	
+		<div class="boxHeader">Melprints Box Options</div>
+        
+        <div class="boxTypeSection">
+        	<div class="boxTypeImages" style="align:left; text-align:center;"><img src="images/pizzaBox.png" width="300" height="300"/></div>
+        	<div class="boxTypeImages" style="align:left; text-align:center;"><img src="images/rettLocks.png" width="300" height="300"/></div>
+        	<div class="boxTypeImages" style="align:left; text-align:center;"><img src="images/tuckTopWithFlap.png" width="300" height="300"/></div>
         	
-            <div class="boxSection">
-            	<div class="boxImages"><img src="images/rettLocks.png"/></div>
-				<div class="boxDescription">Roll End Tuck Top with Locks</div>
-			</div>
-        	
-            <div class="boxSection">
-            <div class="boxImages"><img src="images/tuckTopWithFlap.jpg"/></div>
-				<div class="boxDescription">Tuck Top with Flap</div>
-			</div>
-            </div>
-        	
-            <div class="boxSection">
-            <div class="boxImages"><img src="images/melprintsBox.jpg"/></div>
-				<div class="boxDescription">Box Example</div>
-                
-			</div>
-            
-        </div><!-- end box wrapper -->
+			       
         <div class="clear"></div>
-</body>
-</html>
+        </div>
 <?php include "../footer.php"; ?>
